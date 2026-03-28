33c7c421dac2f073d9715e58e76d2228fba8f2f8
Michael Xón <159101600+B-Michaelx@users.noreply.github.com>
2026-03-27 23:43:48 -0600

    Add files via upload

---
+++ Tarea8/src/main/java/com/mycompany/tarea8/Tarea8.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.tarea8;
+
+public class Tarea8 {
+
+    public static void main(String[] args) {
+        ConversorTemperatura ventana = new ConversorTemperatura();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
